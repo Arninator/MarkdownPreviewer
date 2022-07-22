40f2ae5d7ccedc73997333a338103fac0283ca60
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-07-22 13:16:53 +0000

    experiment with colors and border styles

---
+++ assets/javaScript.js
@@ -0,0 +1,22 @@
+const JSX = <div><p>BLAVLAS</p></div>
+
+class previewText extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            JSX
+        );
+    }
+
+}
+this.handleClick = this.handleClick.bind(this);
+
+ReactDOM.render(JSX, document.getElementById("root"));
+
+function handleClick() {
+    $("#preview") = new previewText();
+}
+
+$("#editor").onChange(handleClick());
